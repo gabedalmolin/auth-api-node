56e96d77bab09984d6f009db6d0d05a6292eb6a4
John Dalmolin <82729254+john-dalmolin@users.noreply.github.com>
2026-02-10 22:34:50 -0300

    chore(ts): migrate route modules to TypeScript

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-const docsRoutes = require("./routes/docsRoutes");
+const docsRoutes = require("./routes/docsRoutes.ts");
 app.use("/", docsRoutes);
 
 const requestId = require("./middlewares/requestId");
@@ -12,13 +12,13 @@ const loggerMiddleware = require("./middlewares/logger");
 app.use(requestId);
 app.use(loggerMiddleware);
 
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/authRoutes.ts");
 app.use("/auth", authRoutes);
 
-const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("./routes/userRoutes.ts");
 app.use("/users", userRoutes);
 
-const healthRoutes = require("./routes/healthRoutes");
+const healthRoutes = require("./routes/healthRoutes.ts");
 app.use("/", healthRoutes);
 
 const errorHandler = require("./middlewares/errorHandler");
